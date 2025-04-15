9db2d0eb039e12affaf25985c9a9fc692f6e7afb
citraadelia <citraadelia8@gmail.com>
2025-04-15 11:43:55 +0700

    buat tokoku

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:tokoku/screens/home_screen.dart';
+
+void main() 
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tokoku',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.pinkAccent,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          )
+        )
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
